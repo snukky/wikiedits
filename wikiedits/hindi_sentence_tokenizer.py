9904a4d0dd90def0037aa8db2d397cd7a41daca8
sujeet kumar vishwakarma <sujeetkr.vishwakarma.eee16@itbhu.ac.in>
2020-01-19 10:47:19 +0530

    edited hindi sentence tokenizer

--- wikiedits/hindi_sentence_tokenizer.py
+++ wikiedits/hindi_sentence_tokenizer.py
@@ -13,7 +13,18 @@ class HindiSentenceTokenizer:
             splitting_point=min([x for x in pos if not x==-1])
             sentences.append(text[:splitting_point+1])
             text=text[splitting_point+1:]
-            yield sentences[len(sentences)-1]
+            ans=sentences[len(sentences)-1]
+            i=0
+            while i<len(ans) and ans[i]==' ':
+                i=i+1
+            if i<len(ans):
+                ans=ans[i:]
+                i=len(ans)-1
+                while i>0 and ans[i]==' ':
+                    i=i-1
+                if i>0:
+                    ans=ans[:i+1]
+                    yield ans
     
             
         
