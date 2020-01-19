2b5824d5a806f195b4f35c9dccf65a29e42a95aa
ankur <s.ankursonawane@gmail.com>
2020-01-19 11:30:48 +0530

    Autopep

--- tests/test_hindi_sentence_tokenizer.py
+++ tests/test_hindi_sentence_tokenizer.py
@@ -6,38 +6,36 @@ class TestHindiSentenceTokenizer(unittest.TestCase):
 
     def setUp(self):
 
-        self.tokenizer=HindiSentenceTokenizer()
+        self.tokenizer = HindiSentenceTokenizer()
 
     def test_tokenize_simple(self):
         text = """कालिंजर दुर्ग, दुर्ग है। बुन्देलखण्ड  धरोहर स्थल खजुराहो से ९७.७ किमी दूर है। इसे भारत के सजाता रहा है। इस दुर्ग में कई प्राचीन मन्दिर हैं। इनमें कई गुप्तकाल के हैं।  शिव ने यही तपस्या कर उसकी ज्वाला शांत की थी। """
-        sent=["कालिंजर दुर्ग, दुर्ग है।",
-              "बुन्देलखण्ड  धरोहर स्थल खजुराहो से ९७.७ किमी दूर है।",
-              "इसे भारत के सजाता रहा है।",
-              "इस दुर्ग में कई प्राचीन मन्दिर हैं।",
-              "इनमें कई गुप्तकाल के हैं।",
-              "शिव ने यही तपस्या कर उसकी ज्वाला शांत की थी।"]    
-        self.assertListEqual(list(self.tokenizer.tokenize(text)),sent)
+        sent = ["कालिंजर दुर्ग, दुर्ग है।",
+                "बुन्देलखण्ड  धरोहर स्थल खजुराहो से ९७.७ किमी दूर है।",
+                "इसे भारत के सजाता रहा है।",
+                "इस दुर्ग में कई प्राचीन मन्दिर हैं।",
+                "इनमें कई गुप्तकाल के हैं।",
+                "शिव ने यही तपस्या कर उसकी ज्वाला शांत की थी।"]
+        self.assertListEqual(list(self.tokenizer.tokenize(text)), sent)
 
     def test_tokenize_empty(self):
-        text ="   "
-        sent=[]
-        self.assertListEqual(list(self.tokenizer.tokenize(text)),sent)
+        text = "   "
+        sent = []
+        self.assertListEqual(list(self.tokenizer.tokenize(text)), sent)
 
     def test_tokenize_other(self):
-        text ="कालिंजर दुर्ग, भारतीय राज्य उत्तर प्रदेश के बांदा! विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर!? विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर!"
-        sent=["कालिंजर दुर्ग, भारतीय राज्य उत्तर प्रदेश के बांदा!",
-              "विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर!?",
-              "विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर"]
-        self.assertListEqual(list(self.tokenizer.tokenize(text)),sent)
+        text = "कालिंजर दुर्ग, भारतीय राज्य उत्तर प्रदेश के बांदा! विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर!? विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर!"
+        sent = ["कालिंजर दुर्ग, भारतीय राज्य उत्तर प्रदेश के बांदा!",
+                "विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर!?",
+                "विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर"]
+        self.assertListEqual(list(self.tokenizer.tokenize(text)), sent)
 
     def test_tokenize_space(self):
-        text="     विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर ! ? "
-        sent=["विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर ! ?"]
-        self.assertListEqual(list(self.tokenizer.tokenize(text)),sent)
-
+        text = "     विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर ! ? "
+        sent = ["विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर ! ?"]
+        self.assertListEqual(list(self.tokenizer.tokenize(text)), sent)
 
     def test_tokenize_noend(self):
-        text="विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर"
-        sent=["विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर ! ?"]
-        self.assertListEqual(list(self.tokenizer.tokenize(text)),sent)
-
+        text = "विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर"
+        sent = ["विंध्य पर्वत पर स्थित यह दुर्ग विश्व धरोहर ! ?"]
+        self.assertListEqual(list(self.tokenizer.tokenize(text)), sent)
