13caab8802a4e20d36d2b63d6d0a201908983515
ankur <s.ankursonawane@gmail.com>
2020-01-21 00:12:51 +0530

    Reverts for  metadata test failure

--- wikiedits/edit_extractor.py
+++ wikiedits/edit_extractor.py
@@ -13,9 +13,10 @@ class EditExtractor:
     def extract_edits(self, old_text, new_text):
         frags = self.diff.edited_fragments(old_text.split("\n"),
                                            new_text.split("\n"))
+        # Generator is not used as it doesn't allow to check how many edits
+        # have been returned.
         try:
-            for frag_pair in frags:
-                for edit in self.filter.filter_edits(*frag_pair):
-                    yield edit
+            return [edit for frag_pair in frags
+                         for edit in self.filter.filter_edits(*frag_pair)]
         except:
-            return
+            return []
