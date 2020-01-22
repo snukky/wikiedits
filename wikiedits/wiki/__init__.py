9f2cd4ee0f0936ef09bc3976cc93b1902621c49a
ankur <s.ankursonawane@gmail.com>
2020-01-22 21:51:02 +0530

    Bugfix: Added Punjabi and bengali vandalism regex

--- wikiedits/wiki/__init__.py
+++ wikiedits/wiki/__init__.py
@@ -4,5 +4,7 @@ VANDALISM_REGEXES = {
     'english': "vandal|stupid|revert",
     'polish': "anulowan|wycofan|cofnię|cofnie|przywróc|przywroc|revert|rewert",
     'german': "revert|vandal|rückgängig",
-    'hindi': "revert|पूर्ववत"
+    'hindi': "revert|पूर्ववत",
+    'bengali': "revert|বাতিল",
+    'punjabi' :"revert|ਸੋਧਾਂ ਵਾਪਸ|ਨਕਾਰੀ"
 }
