1e86b0c6846d350effad0d3d4ee2ecb6699a3d95
ankur <s.ankursonawane@gmail.com>
2020-01-22 21:06:24 +0530

    Added to Vandalism regex

--- wikiedits/wiki/__init__.py
+++ wikiedits/wiki/__init__.py
@@ -4,5 +4,5 @@ VANDALISM_REGEXES = {
     'english': "vandal|stupid|revert",
     'polish': "anulowan|wycofan|cofnię|cofnie|przywróc|przywroc|revert|rewert",
     'german': "revert|vandal|rückgängig",
-    'hindi': "पूर्ववत"
+    'hindi': "revert|पूर्ववत"
 }
